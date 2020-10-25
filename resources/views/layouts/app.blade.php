7e0210d26e9f78b129e0039c12c4cc2b4c6d9e5f
Szymon Gackowski <szymon.gackowski@gmail.com>
2020-10-25 17:17:29 +0100

    layout translations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,11 +38,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">Zaloguj</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">Zarejestruj</a>
                                 </li>
                             @endif
                         @else
@@ -56,7 +56,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        Wyloguj
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
