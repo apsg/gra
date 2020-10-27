aef61d3f011f7d31a37ec596c634127a1d39eeaf
Szymon Gackowski <szymon.gackowski@gmail.com>
2020-10-27 15:17:02 +0100

    Fix colors

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -393,7 +393,8 @@
     @stack('styles')
 </head>
 <body class="antialiased">
-<div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+<div id="app"
+     class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0 bg-blue">
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
@@ -412,7 +413,7 @@
         @yield('content')
     </div>
 </div>
-<script src="{{ asset('app.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
 @stack('scripts')
 </body>
 </html>
